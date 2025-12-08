d4cead4890d777b6efa62d84ef8dc934927c16c1
Ali Mohamed Mansor <137876199+aliMohamed2345@users.noreply.github.com>
2025-12-08 19:18:35 +0200

    add some cors and helmet for security rate limit for api security and organize the code structure

--- src/utils/env-config.ts
+++ src/utils/env-config.ts
@@ -9,14 +9,17 @@ type ConfigType = {
   cloud_name: string;
   api_key: string;
   api_secret: string;
+  frontend_url: string;
 };
 
 export const config: ConfigType = {
   port: +process.env.PORT!,
   connectionString: process.env.DATABASE_URL!,
   jwtSecret: process.env.JWT_SECRET!,
-  nodeEnv: process.env.NODE_ENV === "development" ? "development" : "production",
+  nodeEnv:
+    process.env.NODE_ENV === "development" ? "development" : "production",
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME ?? "",
   api_key: process.env.CLOUDINARY_API_KEY ?? "",
   api_secret: process.env.CLOUDINARY_API_SECRET ?? "",
+  frontend_url: process.env.FRONTEND_URL ?? "",
 };
